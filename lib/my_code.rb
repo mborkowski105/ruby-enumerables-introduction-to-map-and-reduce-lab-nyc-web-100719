a2d3f2b3c46552f329a80845b1b3779ad55f2c81
Matthew Borkowski <barnabytheplumber@gmail.com>
2019-10-03 21:22:28 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,30 @@
 # My Code here....
+def map_to_negativize(array)
+  array = array.map { |element| element * -1}
+  
+  return array
+end
+
+def map_to_no_change(array)
+  return array
+end
+
+def map_to_square(array)
+  return array.map { |e| e*e }
+end
+
+def map_to_double(array)
+  return array.map { |e| e*2 }
+end
+
+def reduce_to_total(array, start = 0)
+  return array.reduce(start) { |sum, num| sum + num }
+end
+
+def reduce_to_all_true(array)
+  return array.all?
+end
+
+def reduce_to_any_true(array)
+  return array.any?
+end
