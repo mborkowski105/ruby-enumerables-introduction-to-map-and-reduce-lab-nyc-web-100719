84fd00bf16d537cbb0e585ea89f75bd1c02e2451
Matthew Borkowski <barnabytheplumber@gmail.com>
2019-10-03 21:18:35 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -17,6 +17,6 @@ def map_to_double(array)
   return array.map { |e| e*2 }
 end
 
-def reduce_to_total(array)
-  return array.reduce(0) { |sum, num| sum + num }
+def reduce_to_total(array, start = 0)
+  return array.reduce(start) { |sum, num| sum + num }
 end
